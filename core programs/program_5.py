64430800dc6adc9e78e62da2c8f007b8745fc153
nitinmehra09 <nitinmehra9983@gmail.com>
2024-05-13 16:23:25 +0530

    program_5 is added

---
+++ core programs/program_5.py
@@ -0,0 +1,18 @@
+# variables 
+a = 3
+b = 4
+name = "nitin mehra"
+
+userName ="Nitin Mehra"
+
+user_name = "nitin mehra "
+
+# can't use 
+# $name  = "nitin"          symbol at first place
+
+# 1name = "nitin "          number at first place 
+
+#  a = 3                    space at first place
+
+
+
