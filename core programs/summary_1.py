9970d6e37f908c9ac931871582153960b87176f0
nitinmehra09 <nitinmehra9983@gmail.com>
2024-05-21 17:30:35 +0530

    sum_1 and sum_2 of chapter 1 and two is added

---
+++ core programs/summary_1.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("line A \tline B")
+print("line A \nline B")
+
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(5//3)  #only integer
---
+++ core programs/summary_2.py
@@ -0,0 +1,15 @@
+name = "nitiniiiiiiii"
+# print(name[10::-1])
+# num=input("Enter a number ")
+# username ,age =input("Enter name and age using ,").split(",")
+# upper = name.upper()
+# lower = name.lower()
+# tittle = name.title()
+
+# print(upper + lower + tittle)
+
+print(name.find("i",7))
+
+print(name.center(28,"*"))
+
+print(name.replace("i","t",3))
