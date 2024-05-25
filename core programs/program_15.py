f05e7c0dac8b8702dd9b3e180460ecf809b69e6d
nitinmehra09 <nitinmehra9983@gmail.com>
2024-05-25 14:51:17 +0530

    program_15 is added

---
+++ core programs/program_15.py
@@ -0,0 +1,10 @@
+name = input("Enter your user_name : ")
+age =int(input("Enter your age "))
+
+if (name[0]=='a'or name[0]=='A'):
+    if(age>=10):
+        print("you can watch coco ")
+    else:
+        print("your age is below 10 ")
+else:
+    print("your name not start with 'A' or 'a' so you can not watch coco ")
