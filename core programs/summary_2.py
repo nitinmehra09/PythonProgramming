209af643c56a08903141beb0e33ce81505b6d3a5
nitinmehra09 <nitinmehra9983@gmail.com>
2024-05-26 11:26:10 +0530

    summary of chapter 3 is added

--- core programs/summary_2.py
+++ core programs/summary_2.py
@@ -1,12 +1,12 @@
 name = "nitiniiiiiiii"
-# print(name[10::-1])
-# num=input("Enter a number ")
-# username ,age =input("Enter name and age using ,").split(",")
-# upper = name.upper()
-# lower = name.lower()
-# tittle = name.title()
+print(name[10::-1])
+num=input("Enter a number ")
+username ,age =input("Enter name and age using ,").split(",")
+upper = name.upper()
+lower = name.lower()
+tittle = name.title()
 
-# print(upper + lower + tittle)
+print(f"{upper} + {lower} + {tittle}")
 
 print(name.find("i",7))
 
