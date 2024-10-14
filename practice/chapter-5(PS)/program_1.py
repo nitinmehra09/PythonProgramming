fbb884ac55016a53cdf3a9745359db5b4ee1ac2b
nitinmehra09 <nitinmehra9983@gmail.com>
2024-10-14 17:35:30 +0530

    PS-5 is added

---
+++ practice/chapter-5(PS)/program_1.py
@@ -0,0 +1,9 @@
+words = {
+    "नमस्ते" : "Hello",
+    "धन्यवाद" : "Thank you" ,
+    "कृपया" : "Please",
+    "खाना" : "Food / Meal"
+}      
+word = input("Enter a word i will translate in into english")
+print(words[word])
+
