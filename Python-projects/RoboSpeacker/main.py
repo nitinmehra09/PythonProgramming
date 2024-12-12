2c9ab283c52c63b961829a33df8adbde22b91d51
nitinmehra09 <nitinmehra9983@gmail.com>
2024-12-12 10:11:07 +0530

    New project roboSpeacker is added

---
+++ Python-projects/RoboSpeacker/main.py
@@ -0,0 +1,11 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    while True:
+        print("Welcome to robospeaker 1.1")
+        x = input("Enter what you want me to say: ")
+        if (x=='q'):
+            break
+        engine = pyttsx3.init()
+        engine.say(x)
+        engine.runAndWait()
