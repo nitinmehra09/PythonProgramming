f740551f90c0d7bbb5c3f73120ae09d41ab08cbf
nitinmehra09 <nitinmehra9983@gmail.com>
2024-12-22 22:09:37 +0530

    Some uptation in To-do list

--- Python-projects/To-doList/main.py
+++ Python-projects/To-doList/main.py
@@ -1,20 +1,25 @@
 l = []
 
 while True:
-    print("Enter your Choice : ")
+    print("\n--- Task Manager ---")
     print("1. Add Tasks\n2. Delete Tasks\n3. Veiw All Tasks\n4. Exit ")
     Choice = int(input("Enter your choice : "))
     try:
         if Choice == 1:
             tasks = input("Enter the task : ")
             l.append(tasks)
+            print(f"Task added: {tasks}")
         elif Choice == 2:
-            index = int(input("Enter index of task you want to delete : "))
-            l.remove(l[index-1])
+            if tasks:
+                index = int(input("Enter index of task you want to delete : "))
+                l.remove(l[index-1])
+                print(f"{i}. {tasks}")
+            else:
+                print("To-do list is empty - ")
         elif Choice == 3:
             print("Your Tasks are : ")
             for i in range(0,len(l)):
-                print(l[i])
+                print(f"   {1}. {l[i]}")
         elif Choice == 4:
             print("Thank You !!!")
             break
