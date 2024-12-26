b13d622bc54f8c24d7981b9bca00a9bca6484734
nitinmehra09 <nitinmehra9983@gmail.com>
2024-12-26 22:47:09 +0530

    New project is added whose name is password manager

---
+++ Python-projects/Instagram Automation/main.py
@@ -0,0 +1,12 @@
+# from instabot import Bot
+
+# bot = Bot()
+
+# Bot.login(username="jeonsuax.nis_",password = "QWERTY@123")
+#//88
+# bot.follow("aditimistry2607")
+
+from instabot import Bot
+bot = Bot() 
+bot.login(username="jeonsuax.nis_", password="QWERTY@123")
+bot.follow("aditimistry2607")
