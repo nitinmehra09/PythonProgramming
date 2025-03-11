2c7d7e0e48d0e6e139fac32da9398eb653cc707d
nitinmehra09 <nitinmehra9983@gmail.com>
2025-03-11 11:57:25 +0530

    Operators and Control Flow is added

---
+++ Core Programs/Operators and Control Flow/program_4.py
@@ -0,0 +1,6 @@
+string = input("ENTER A STRING : ")
+if(len(string)<10):
+    print("you enter a string having less 10 char ")
+
+else:
+    print("you enter a string having more then or equal to 10 char ")
---
+++ Core Programs/Operators and Control Flow/program_6.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter your marks : "))      
+
+if (90 <= marks < 100):
+    print("Ex")
+elif (80 <= marks < 90):
+    print("A")
+elif (70 <= marks < 80):
+    print("B")
+elif (60 <= marks < 70):
+    print("C")
+elif (50 <= marks < 60):
+    print("D")
+elif(0<= marks < 50 ):
+    print("F")
+else:
+    print("Enter a valid marks")
